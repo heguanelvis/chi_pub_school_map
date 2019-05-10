ae171a4303960d3f694e57c535f9a6ff69e1fb2c
heguanelvis <heguanelvis@gmail.com>
2019-05-10 01:09:27 -0500

    added basic click popups

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGVndWFuZWx2aXMiLCJhIjoiY2p0cWFnMmR4MGRlOTQ1bXVkNGhqbnYxYiJ9.g43Zo8jIYEj6l-o_3MD3Hg';
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -6,20 +7,35 @@ var map = new mapboxgl.Map({
     center: [-87.6298, 41.8351]
 });
 
-// https://data.cityofchicago.org/api/views/mntu-576c/rows.geojson
 map.on('load', () => {
-    map.addSource('pointsSource', {
+    map.addSource('cpsSource', {
         type: 'geojson',
         data: 'https://data.cityofchicago.org/api/views/mntu-576c/rows.geojson'
     });
 
     map.addLayer({
-        "id": "points",
+        "id": "cpsPoints",
         "type": "circle",
-        "source": "pointsSource",
+        "source": "cpsSource",
         "paint": {
-            "circle-radius": 3,
+            "circle-radius": 5,
             "circle-color": "#007cbf"
         }
+    });
+
+    map.on('click', 'cpsPoints', e => {
+        console.log("clicked")
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.school_nm)
+            .addTo(map);
+    });
+
+    map.on('mouseenter', 'cpsPoints', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'cpsPoints', () => {
+        map.getCanvas().style.cursor = '';
     });
 });
